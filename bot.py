f7eb8d7ef5df5893da6b3854b19d4b313bf0de82
bubus2 <bubus258@gmail.com>
2021-05-31 19:32:28 +0200

    Implemented random response

--- bot.py
+++ bot.py
@@ -5,6 +5,16 @@ from discord.ext import commands
 from image_generator import create_image
 from PIL import Image
 from io import BytesIO
+from random import randint
+
+
+async def response(text, context):
+    print(text)
+    with BytesIO() as image_binary:
+        create_image(text).save(image_binary, 'PNG')
+        image_binary.seek(0)
+        await context.channel.send(file=discord.File(fp=image_binary, filename='image.png'))
+        
 
 def main():
     client = commands.Bot(command_prefix="?")
@@ -19,12 +29,11 @@ def main():
     async def on_message(ctx):
         if(ctx.content.startswith(":ok") and ctx.content[-1] == ":"):
             arg = ctx.content[3:-1]
-            print(arg)
-            with BytesIO() as image_binary:
-                create_image(arg).save(image_binary, 'PNG')
-                image_binary.seek(0)
-                await ctx.channel.send(file=discord.File(fp=image_binary, filename='image.png'))
-                await ctx.delete()
+            await response(arg, ctx)
+            await ctx.delete()
+        elif (randint(1, 2000) >= 1999 and ctx.author != client.user):
+            nickname = str(ctx.author)[0:-5]
+            await response(nickname, ctx)
 
     client.run(os.getenv("DISCORD_TOKEN"))
 
