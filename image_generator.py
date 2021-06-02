f0610eb4e01a36ba7a53c09f4386ce37df132e22
td236 <dadela.tomasz@gmail.com>
2021-06-03 00:00:46 +0200

    simple height scaling

--- image_generator.py
+++ image_generator.py
@@ -5,9 +5,7 @@ import requests
 # API url with slash!
 API_URL = "http://skyman503.pythonanywhere.com/"
 
-def center_text(img, font, text, color=(255, 255, 255)):
-    W = 1000
-    H = 200
+def center_text(img, font, text, W, H, color=(255, 255, 255)):
     draw = ImageDraw.Draw(img)
     text_width, text_height = draw.textsize(text, font)
     position = ((W-text_width)/2,(H-text_height)/2)
@@ -17,9 +15,9 @@ def center_text(img, font, text, color=(255, 255, 255)):
 
 def create_image(text, author):
     W = 1000
-    H = 200
+    H = 200 * int((1 + text.count('\n') / 3))
     text = "Ok " + str(text)
-    
+
     background = get_background(author.id)
     if (background == None):
         img = Image.new('RGB', (W, H))
@@ -32,15 +30,20 @@ def create_image(text, author):
     img_fraction = 0.8
 
     font = ImageFont.truetype("fonts/font.otf", fontsize)
-    while font.getsize(text)[0] < img_fraction*img.size[0]:
+    draw = ImageDraw.Draw(img)
+    text_width, text_height = draw.textsize(text, font)
+
+    while text_width < W * img_fraction and text_height < H * img_fraction:
+        #print(font.getsize(text)[0], W *0.8, font.getsize(text)[1], H * 0.8)
         # iterate until the text size is just larger than the criteria
         fontsize += 1
+        text_width, text_height = draw.textsize(text, font)
         font = ImageFont.truetype("fonts/font.otf", fontsize)
 
-    fontsize = min(60, fontsize)
+    #print(fontsize)
     font = ImageFont.truetype("fonts/font.otf", fontsize)
 
-    center_text(img, font, str(text))
+    center_text(img, font, str(text), W, H)
 
     img.paste(ok_emoji, (10, 75))
     return img
@@ -52,7 +55,7 @@ def get_background(id):
     except requests.ConnectionError:
         print("ERROR: Website does not exist!")
         return
-    
+
     if response.status_code == 200:
         return response.content
     else:
