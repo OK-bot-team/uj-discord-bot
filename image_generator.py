75e08109b02ea4714b3d16f39b2fed172d0f2fea
td236 <dadela.tomasz@gmail.com>
2021-06-03 13:43:15 +0200

    image generator: smaller font size for long text

--- image_generator.py
+++ image_generator.py
@@ -6,24 +6,26 @@ import requests
 API_URL = "http://skyman503.pythonanywhere.com/"
 
 
-def center_text(img, font, text, W, H, color=(255, 255, 255)):
+def get_text_dimensions(text, font):
+    img = Image.new('RGB', (1, 1))
     draw = ImageDraw.Draw(img)
-    text_width, text_height = draw.textsize(text, font)
-    position = ((W - text_width) / 2, (H - text_height) / 2)
-    draw.text(position, text, color, font=font)
-    return img
+    return draw.textsize(text, font)
 
 
 def create_image(text, author):
     fontsize = 90
+    if (len(text) > 100):
+        fontsize = 45
+    color = (255, 255, 255)
+    font = ImageFont.truetype("fonts/font.otf", fontsize)
     text = "Ok " + str(text)
 
-    font = ImageFont.truetype("fonts/font.otf", fontsize)
-    img = Image.new('RGB', (100, 100))
-    draw = ImageDraw.Draw(img)
-    text_width, text_height = draw.textsize(text, font)
+    text_width, text_height = get_text_dimensions(text, font)
     W = max(1000, int(text_width * 1.1) + 30)
     H = max(200, int(text_height * 1.1) + 30)
+    print(text_width, text_height)
+    print("Image size: ", W, H,)
+    position = ((W - text_width) / 2, (H - text_height) / 2)
 
     background = get_background(author.id)
     if (background is None):
@@ -32,8 +34,8 @@ def create_image(text, author):
         img = Image.open(BytesIO(background))
         img = img.resize((W, H))
 
-    font = ImageFont.truetype("fonts/font.otf", fontsize)
-    center_text(img, font, str(text), W, H)
+    draw = ImageDraw.Draw(img)
+    draw.text(position, text, color, font=font)
 
     ok_emoji = Image.open("images/ok.png")
     img.paste(ok_emoji, (10, 75))
