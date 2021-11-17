72c084568118241723b56793f2fb1f7dbcae8b18
koostosh <koostosh@gmail.com>
2021-11-17 11:46:47 +0100

    add zdalne functionality

--- bot.py
+++ bot.py
@@ -10,21 +10,29 @@ import re
 import asyncio
 
 
-async def response(text, context, author, raw):
+async def response(text, context, author, count):
     print("Message text: " + text[:50])
     with BytesIO() as image_binary:
-        create_image(text, author, raw).save(image_binary, 'PNG')
+        create_image(text, author, count).save(image_binary, 'PNG')
         image_binary.seek(0)
         await context.channel.send(file=discord.File(fp=image_binary, filename='image.png'))
 
+#returns text, number of escape symbols
+# 0- delete original message
+# 1- delete original and do not prepend "ok"
+# 2- delete original and do not prepend "ok" nor emoji
+# 3- do not delete and do not prepend "ok" nor emoji
 def get_text(message, author = None):
-    regx = re.search(r"(?<=;[oO][kK])([\s\S]*)(?=;)", message);
+    regx = re.search(r"(?<=;[oO][kK])(~*)([\s\S]*)(?=;)", message);
     if regx != None:
-        return regx.group(0), False
+        return regx.group(2), len(regx.group(1))
     regx = re.search(r"([\s\S]*) [bB]ocie", message);
     if regx != None:
-        return regx.group(1) + " " + author, True
-    return None, False
+        return regx.group(1) + " " + author, 3
+    regx = re.search(r"(kiedy|where) zdalne", message);
+    if regx != None:
+        return "nie ma żadnych zdalnych, zdalne wyśliliście sobie wy, studenci", 3
+    return None, 0
 
 def main():
     client = commands.Bot(command_prefix="?")
@@ -41,14 +49,14 @@ def main():
         await client.process_commands(ctx)
 
         nickname = str(ctx.author)[0:-5]
-        text, raw = get_text(ctx.content, nickname)
+        text, count = get_text(ctx.content, nickname)
 
         if(text):
-            await response(text, ctx, ctx.author, raw)
-            if not raw:
+            await response(text, ctx, ctx.author, count)
+            if count <= 2:
                 await ctx.delete()
         elif (randint(1, 2000) >= 1999 and ctx.author != client.user):
-            await response(nickname, ctx, ctx.author, raw)
+            await response(nickname, ctx, ctx.author, count)
 
     @client.command(brief="Website to change your background")
     async def background(ctx):
